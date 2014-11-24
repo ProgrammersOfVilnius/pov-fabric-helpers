5cc9dec1213999bbe370af5ca9c4bdddff9061f5
Marius Gedminas <marius@gedmin.as>
2014-11-24 10:30:48 +0200

    Tests for instance management stuff

--- tests.py
+++ tests.py
@@ -4,6 +4,12 @@ Tests for pov_fabric.
 Run them with nose.
 """
 
+from cStringIO import StringIO
+
+import mock
+from nose.tools import assert_raises
+from fabric.utils import _AttributeDict
+
 from pov_fabric import *  # noqa
 
 
@@ -61,3 +67,48 @@ def test_Instance():
     instance = Instance("test", "localhost")
     assert "{name} on {host}".format(**instance) == "test on localhost"
 
+
+def test_Instance_asdict():
+    instance = Instance("test", "localhost")
+    assert instance._asdict() == {'name': 'test', 'host': 'localhost'}
+
+
+def test_Instance_with_params():
+    MyInstance = Instance.with_params(
+        color=Instance.REQUIRED,
+        shape="circle",
+    )
+    instance = MyInstance("test", "localhost", color="red", shape="square")
+    assert instance.color == "red"
+    assert instance.shape == "square"
+
+    instance = MyInstance("test", "localhost", color="red")
+    assert instance.shape == "circle"
+
+    assert_raises(TypeError, MyInstance, "test", "localhost")
+    assert_raises(TypeError, MyInstance, "test", "localhost", color="red",
+                  nonsense="not allowed")
+
+
+@mock.patch("sys.stderr", new_callable=StringIO)
+@mock.patch("pov_fabric.env", new_callable=_AttributeDict)
+def test_Instance_definition_and_management(env, stderr):
+    Instance.define("test", "localhost")
+    assert env.instances["test"] == Instance("test", "localhost")
+    Instance.define("another", "localhost")
+    assert env.instances["another"] == Instance("another", "localhost")
+    assert_raises(SystemExit, Instance.define, "test", "again")
+    assert "Instance test is already defined." in stderr.getvalue()
+
+    test()  # global magically defined by Instance.define()
+    assert env.instance == 'test'
+
+    env.command = 'triangulate'
+    assert get_instance() == Instance("test", "localhost")
+    assert get_instance("another") == Instance("another", "localhost")
+    assert_raises(SystemExit, get_instance, "nonesuch")
+    assert "Please specify an instance (another, test), e.g.\n\n  fab another triangulate" in stderr.getvalue()
+
+    del env['instances']
+    assert_raises(SystemExit, get_instance, "test")
+    assert "There are no instances defined in env.instances" in stderr.getvalue()
